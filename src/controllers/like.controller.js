071674bd322b57167b0d3b0664ed0a96186f45e7
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-06-06 15:44:33 +0530

    added getLikedVideos controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,4 +1,4 @@
-import { isValidObjectId } from 'mongoose';
+import mongoose, { isValidObjectId } from 'mongoose';
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { Like } from '../models/like.model.js';
 import { ApiResponse } from "../utils/apiResponse.js"
@@ -98,47 +98,121 @@ export const commentLike = asyncHandler(async (req, res) => {
         )
 });
 
-export const tweetLike = asyncHandler(async(req, res)=>{
-    const {tweetId} = req.params;
+export const tweetLike = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
 
-    if(!tweetId){
+    if (!tweetId) {
         throw new ApiError(400, "tweetId is required")
     }
 
-    if(!isValidObjectId(tweetId)){
+    if (!isValidObjectId(tweetId)) {
         throw new ApiError(400, "tweetId is not valid")
     }
 
-    const alreadyLiked  = await Like.findOne(
+    const alreadyLiked = await Like.findOne(
         {
             tweet: tweetId,
             likedBy: req.user._id
         }
     )
-    if(alreadyLiked){
+    if (alreadyLiked) {
         await Like.findByIdAndDelete(alreadyLiked?._id);
 
-        return res 
-        .status(200)
-        .json(
-            new ApiResponse(200, {isLiked: false}, "tweet disliked successfully")
-        )
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, { isLiked: false }, "tweet disliked successfully")
+            )
     }
 
     const likeTweet = await Like.create(
         {
-            tweet:tweetId,
+            tweet: tweetId,
             likedBy: req.user._id
         }
     )
 
-    if(!likeTweet){
+    if (!likeTweet) {
         throw new ApiError(500, "server error: tweet could not be liked")
     }
 
-    return res 
-    .status(200)
-    .json(
-        new ApiResponse(200, {likeTweet}, "tweet liked successfully")
-    )
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { likeTweet }, "tweet liked successfully")
+        )
+});
+
+export const getLikedVideos = asyncHandler(async (req, res) => {
+    const userId = req.user?._id;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "userId is not valid")
+    }
+
+    const allLikedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "videoOwner",
+                            foreignField: "_id",
+                            as: "videoOwner"
+                        }
+                    },
+                    {
+                        $unwind: "$videoOwner"
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$likedVideos"
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                likedVideos: {
+                    _id: 1,
+                    title: 1,
+                    description: 1,
+                    videoFile: 1,
+                    thumbnail: 1,
+                    duration: 1,
+                    views: 1,
+                    createdAt: 1,
+                    videoOwner: {
+                        username: 1,
+                        fullname: 1,
+                        avatar: 1
+                    }
+                }
+            }
+        }
+    ])
+
+    if (!allLikedVideos || allLikedVideos.length === 0) {
+        throw new ApiError(404, "No liked videos found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, allLikedVideos, "videos fetched successfully")
+        )
 })
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { commentLike, tweetLike, videoLike } from "../controllers/like.controller.js";
+import { commentLike, getLikedVideos, tweetLike, videoLike } from "../controllers/like.controller.js";
 
 
 const router = Router();
@@ -10,5 +10,7 @@ router.route("/like-video/:videoId").post(verifyJwt , videoLike);
 router.route("/like-comment/:commentId").post(verifyJwt , commentLike);
 
 router.route("/like-tweet/:tweetId").post(verifyJwt , tweetLike);
+
+router.route("/get-All-LikedVideos").get(verifyJwt , getLikedVideos)
 
 export default router;
